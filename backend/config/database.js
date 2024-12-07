418d915e22c93a2a9bb9f03eb482332193a78162
Gabriel Coelho <gsc4096@gmail.com>
2024-12-07 19:04:00 -0300

    feat: automatização da criação do banco, substituição do pacote mysql para mysql2

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,18 +1,42 @@
-const mysql = require('mysql');
+const mysqlp = require('mysql2/promise');
+const mysql = require('mysql2');
+const fs = require('fs/promises');
+const path = require('path');
 
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '', // Adicione sua senha, se necessário
-    database: 'gamejam',
-});
+const SQL_FILE_PATH = path.join(process.cwd(), 'config/db.sql');
 
-db.connect(err => {
-    if (err) {
-        console.error('Erro ao conectar ao banco de dados:', err);
-        return;
+async function setupDatabase() {
+    const connection = await mysqlp.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+    });
+    try {
+        const sql = await fs.readFile(SQL_FILE_PATH, 'utf8');
+        const queries = sql.split(';').map(q => q.trim()).filter(q => q);
+        for (const query of queries) await connection.query(query);
+        //const [rows] = await connection.query('SELECT * FROM jogo');
+        //console.log(rows);
+    } catch (error) {
+        console.error(error);
+    } finally {
+        await connection.end();
     }
-    console.log('Conectado ao banco de dados MySQL.');
-});
+}
 
-module.exports = db;
+async function main() {
+    await setupDatabase();
+    const db = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'gamejam',
+    });
+    db.connect(err => {
+        if (err) console.error(err);
+        else console.log('Conectado ao banco.');
+    });
+    module.exports = db;
+}
+
+main().catch(console.error);
